e764d5922bbba6fd2e307dc77b63aa9408d7c7fe
Syaza <syazakurnia@gmail.com>
2026-01-25 22:05:49 +0800

    Rename main class and add greeting

---
+++ src/main/java/MusangKing.java
@@ -0,0 +1,11 @@
+public class MusangKing {
+    public static void main(String[] args) {
+        String line = "_".repeat(60) + "\nw";
+        System.out.println(line +
+                "Hello! I'm MusangKing :)\n" +
+                "What can I do for you?\n" +
+                line +
+                "Bye. Hope to see you again soon!\n" +
+                line);
+    }
+}
