d8754815c7b5dcbe88816c62c0c9568829d85007
Syaza <syazakurnia@gmail.com>
2026-01-25 22:41:38 +0800

    Add support for echo

--- src/main/java/MusangKing.java
+++ src/main/java/MusangKing.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
+
 public class MusangKing {
     public static void main(String[] args) {
-        String line = "_".repeat(60) + "\nw";
+        /*
+         * Chat starts with a greeting message.
+         */
+        String line = "_".repeat(60) + "\n";
         System.out.println(line +
                 "Hello! I'm MusangKing :)\n" +
                 "What can I do for you?\n" +
-                line +
-                "Bye. Hope to see you again soon!\n" +
-                line);
+                line
+        );
+
+        /*
+         * Initialize Scanner object and boolean flag
+         * to enable the chat loop.
+         */
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            String userInput = sc.nextLine(); // reads user input
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(line +
+                        "Bye! Come back, okay?\n" +
+                        line
+                );
+                flag = false;
+            } else {
+                System.out.println(line +
+                        userInput + " ;p\n"+
+                        line
+                );
+            }
+        }
     }
 }
