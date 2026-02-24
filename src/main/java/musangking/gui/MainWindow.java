923ae95d9171ae1ab2c968980635ed8a19614d1b
Syaza <syazakurnia@gmail.com>
2026-02-24 08:25:38 +0800

    Change Main Window color palette

--- src/main/java/musangking/gui/MainWindow.java
+++ src/main/java/musangking/gui/MainWindow.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
